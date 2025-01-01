c4fd5a6a6af792ed01a70e87ccbff2f7b1d0ffdd
AlirezaBelal <belal.alireza@yahoo.com>
2025-01-01 16:15:38 +0330

    refactor code

---
+++ src/utils.php
@@ -0,0 +1,43 @@
+<?php
+
+function sendMessage($chat_id, $text) {
+    $url = API_URL . "sendMessage";
+    $data = [
+        'chat_id' => $chat_id,
+        'text' => $text,
+        'parse_mode' => 'Markdown',
+        'disable_web_page_preview' => true,
+    ];
+    $options = [
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => http_build_query($data),
+    ];
+    $ch = curl_init();
+    curl_setopt_array($ch, $options);
+    $response = curl_exec($ch);
+    curl_close($ch);
+}
+
+function ensureUserExists($chat_id) {
+    global $database;
+
+    $userExists = $database->has("users", ["chat_id" => $chat_id]);
+
+    if (!$userExists) {
+        $database->insert("users", [
+            "chat_id" => $chat_id,
+            "created_at" => Medoo::raw('CURRENT_TIMESTAMP')
+        ]);
+    }
+
+    $userStateExists = $database->has("user_states", ["chat_id" => $chat_id]);
+
+    if (!$userStateExists) {
+        $database->insert("user_states", [
+            "chat_id" => $chat_id,
+            "state" => 'new_user',
+        ]);
+    }
+}
