183e28a366c4ce92b918f459439c105a449d6df8
AlirezaBelal <belal.alireza@yahoo.com>
2025-01-01 15:22:54 +0330

    update courses by telegram id

--- commands/courses.php
+++ commands/courses.php
@@ -3,14 +3,16 @@
 sendMessage($chat_id, "
 در حال حاضر، ما سه دوره آموزشی داریم:
 
-1. **هوش مصنوعی در خدمت مدیران محصول**  
-   [لینک به دوره](https://bozhan.school/courses/aiforpms/)
+1. **دوره مدیریت محصول دیجیتال**  
+   [لینک به دوره](https://bozhan.school/courses/productmanagement/) - [لینک بات](http://t.me/pm_aculearn_bot)
+      
+2. **هوش مصنوعی در خدمت مدیران محصول**  
+   [لینک به دوره](https://bozhan.school/courses/aiforpms/) - [لینک بات](http://t.me/ai_pm_aculearn_bot)
 
-2. **محصول و کسب‌وکار برای متخصصین منابع انسانی**  
-   [لینک به دوره](https://bozhan.school/courses/businessforhr/)
+3. **محصول و کسب‌وکار برای متخصصین منابع انسانی**  
+   [لینک به دوره](https://bozhan.school/courses/businessforhr/) - [لینک بات](http://t.me/hr_pm_aculearn_bot)
 
-3. **دوره مدیریت محصول دیجیتال**  
-   [لینک به دوره](https://bozhan.school/courses/productmanagement/)
+به منظور آشنایی با ماژول‌های اصلی هر دوره، اساتید، سرفصل‌ها و کسب اطلاعات بیشتر پیرامون هر یک از دوره‌های فوق، لطفاً روی لینک اختصاصی بات دوره کلیک کنید.
 
-اگر با دوره‌های بالا آشنایی ندارید، لطفاً از طریق [آزمون دانش](https://t.me/acuLearn_bot?start=interview) نیاز آموزشی خود را به ما اعلام کنید تا سرفصل دوره مناسب برای شما مشخص شود.
+در صورتی که از دانشجویان یکی از دوره‌های فوق هستید، به منظور نیازسنجی و طراحی رودمپ اختصاصی، لطفاً به [لینک نیازسنجی](http://t.me/interview_aculearn_bot) مراجعه فرمایید.
 ");
